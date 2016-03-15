e827e96abc2e046a901e9d86fa425e8078ff31dd
zhuanghua <30758919@qq.com>
2016-03-15 09:08:29 +0800

    since

--- src/main/python/pylogspout/ContainerLog.py
+++ src/main/python/pylogspout/ContainerLog.py
@@ -4,6 +4,7 @@ Created on Mar 11, 2016
 @author: aadebuger
 '''
 from docker import Client
+from datetime import datetime
 import os
 DOCKER_HOST = os.getenv('DOCKER_HOST', 'unix:///var/run/docker.sock')
 
@@ -12,7 +13,7 @@ def logContainer():
     print('cli',cli)
     cli.images()
     line=""
-    for c in cli.logs("mongodb0", stderr=False, stream=True):
+    for c in cli.logs(os.getenv("logcontainer","mongodb0"), stderr=False, stream=True,since=datetime.now() ):
         line=line+c
         if c=='\n':
             print 'line=',line
